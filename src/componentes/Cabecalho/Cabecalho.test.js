76d2bdc14ad931198650e09086ffbaed4608edee
Marcelo Silva <marcelo.silva@lello.com.br>
2023-10-28 20:04:44 -0300

    commit inicial

---
+++ src/componentes/Cabecalho/Cabecalho.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Cabecalho from './index';
+
+test('Deve renderizar o nome do usuário logado', () => {
+  render(<Cabecalho />);
+  const nomeUsuario = screen.getByText('Joana Fonseca Gomes');
+  expect(nomeUsuario).toBeInTheDocument();
+});
---
+++ src/componentes/Extrato/Extrato.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import Extrato from './index';
+
+test('Deve renderizar uma lista de transações', () => {
+
+  const transacoes = [
+    {
+      transacao: 'Depósito',
+      valor: 100,
+    },
+  ];
+
+  render(<Extrato transacoes={transacoes} />);
+  const lista = screen.getByRole('list');
+  expect(lista).toBeInTheDocument();
+});
